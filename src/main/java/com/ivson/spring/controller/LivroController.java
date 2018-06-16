f70e1f99b8ee476427efdf88365bc37daa5ebbec
Ivson Santos <santosivson@hotmail.com>
2018-06-16 13:12:48 -0300

    Projeto criado

--- src/main/java/com/ivson/spring/controller/LivroController.java
+++ src/main/java/com/ivson/spring/controller/LivroController.java
@@ -5,15 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ivson.spring.model.Livro;
 import com.ivson.spring.repository.LivroRepository;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/livros")
 public class LivroController {
 
 	private LivroRepository livroRepository;
@@ -22,8 +23,17 @@ public class LivroController {
     public LivroController( LivroRepository livroRepository) {
           this.livroRepository = livroRepository;
     }
+    
+    @GetMapping("/")
+    public String listaTodos(Model model) {
+          List<Livro> listaLivros = livroRepository.findAll();
+          if (listaLivros != null) {
+                model.addAttribute("livros", listaLivros);
+          }
+          return "listaLivros";
+    }
 
-    @RequestMapping(value = "/{autor}", method = RequestMethod.GET)
+    @GetMapping("/{autor}")
     public String listaLivros(@PathVariable("autor") String autor, Model model) {
           List<Livro> listaLivros = livroRepository.findByAutor(autor);
           if (listaLivros != null) {
@@ -32,10 +42,10 @@ public class LivroController {
           return "listaLivros";
     }
 
-    @RequestMapping(value = "/{autor}", method = RequestMethod.POST)
+    @PostMapping("/{autor}")
     public String adicionaLivroAutor(@PathVariable("autor") String autor, Livro livro) {
           livro.setAutor(autor);
           livroRepository.save(livro);
-          return "redirect:/{autor}";
+          return "redirect:/livros/{autor}";
     }
 }
